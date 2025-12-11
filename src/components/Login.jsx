58fe9a5e1a6b44dcc666fe1df9725034f9800497
Anugrah P <anugrahpchandrodayam@gmail.com>
2025-12-11 21:15:35 +0530

    Dynamic Navbar and Refactoring of code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
 const [emailId, setEmailId]=useState("");
 const [password, setPassword]=useState("");
+const dispatch= useDispatch();
+const navigate= useNavigate();
 
 const handleLogin= async()=>{
     try{
-        const response= await axios.post("http://localhost:3000/login",
+        const res= await axios.post(BASE_URL+"/login",
         {
             emailId,
             password
         },
         {withCredentials: true}
     );
+
+    dispatch(addUser(res.data));
+    navigate("/");
 }
 catch(err){
     console.log(err);
