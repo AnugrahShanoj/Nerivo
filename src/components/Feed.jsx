73a96450eac03f3242b2e3b7d66c768e929633f2
Anugrah P <anugrahpchandrodayam@gmail.com>
2026-01-18 19:20:29 +0530

    Implemented Feed Interested or Ignored Functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -24,6 +24,10 @@ export const Feed = () => {
   useEffect(()=>{
     getFeed();
   },[])
+
+  if(!feed) return ;
+
+  if(feed.length <= 0) return <h1 className='text-center font-bold text-2xl my-10'>No new users found!...</h1>
   return (
     feed && (
       <div className='flex justify-center my-10'>
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,22 @@
+import axios from "axios";
 import React from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
 const UserCard = ({user}) => {
-    const {firstName, lastName, age, gender, photoURL, skills}= user;
+    const {_id, firstName, lastName, age, gender, photoURL, skills}= user;
+    const dispatch= useDispatch();
+
+    const handleSendRequest= async(status, userId)=>{
+      try{
+        const res= await axios.post(BASE_URL+"/request/send/"+status+"/"+userId, {}, {withCredentials:true});
+        console.log(res);
+        dispatch(removeUserFromFeed(userId));
+      }catch(err){
+        console.log(err);
+      }
+    }
   return (
     <div className="card bg-base-300 w-96 shadow-sm p-5">
       <figure>
@@ -15,8 +30,8 @@ const UserCard = ({user}) => {
         <p>{skills}</p>
         {age && gender && <p>{age + ", " + gender}</p>}
         <div className="card-actions justify-center my-6">
-        <button className="btn btn-primary me-4">Ignored</button>
-          <button className="btn btn-secondary">Interested</button>
+        <button className="btn btn-primary me-4" onClick={()=>handleSendRequest("ignored", _id)}>Ignored</button>
+          <button className="btn btn-secondary" onClick={()=>handleSendRequest("interested", _id)}>Interested</button>
         </div>
       </div>
     </div>
